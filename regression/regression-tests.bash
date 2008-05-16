e1fa809e095c5cfc6f47ce63880854831cae25ea
Carl Byington <carl@five-ten-sg.com>
2008-05-16 09:00:57 -0700

    fix documentation of 8 byte backpointers

--- regression/regression-tests.bash
+++ regression/regression-tests.bash
@@ -8,18 +8,20 @@ for i in {1..10}; do
     mkdir output$i
 done
 
-hash=$(md5sum ams.pst)
-pre="$hash
-bates-"
-$val  ../src/pst2dii  -f /usr/share/fonts/bitstream-vera/VeraMono.ttf -B "$pre" -o output1 -O mydii -d dumper ams.pst
-      ../src/readpstlog -f I dumper >ams.log
-$val  ../src/pst2dii  -f /usr/share/fonts/bitstream-vera/VeraMono.ttf -B "bates-" -o output2 -O mydii2 -d dumper sample_64.pst
-      ../src/readpstlog -f I dumper >sample_64.log
-$val  ../src/pst2dii  -f /usr/share/fonts/bitstream-vera/VeraMono.ttf -B "bates-" -o output3 -O mydii3 -d dumper test.pst
-      ../src/readpstlog -f I dumper >test.log
-      ../src/pst2dii  -f /usr/share/fonts/bitstream-vera/VeraMono.ttf -B "bates-" -o output4 -O mydii4 -d dumper big_mail.pst
-      ../src/readpstlog -f I dumper >big_mail.log
-exit
+if [ "$1" == "dii" ]; then
+    hash=$(md5sum ams.pst)
+    pre="$hash
+    bates-"
+    $val  ../src/pst2dii  -f /usr/share/fonts/bitstream-vera/VeraMono.ttf -B "$pre" -o output1 -O mydii -d dumper ams.pst
+          ../src/readpstlog -f I dumper >ams.log
+    $val  ../src/pst2dii  -f /usr/share/fonts/bitstream-vera/VeraMono.ttf -B "bates-" -o output2 -O mydii2 -d dumper sample_64.pst
+          ../src/readpstlog -f I dumper >sample_64.log
+    $val  ../src/pst2dii  -f /usr/share/fonts/bitstream-vera/VeraMono.ttf -B "bates-" -o output3 -O mydii3 -d dumper test.pst
+          ../src/readpstlog -f I dumper >test.log
+          ../src/pst2dii  -f /usr/share/fonts/bitstream-vera/VeraMono.ttf -B "bates-" -o output4 -O mydii4 -d dumper big_mail.pst
+          ../src/readpstlog -f I dumper >big_mail.log
+    exit
+fi
 
 $val  ../src/pst2ldif -b 'o=ams-cc.com, c=US' -c 'newPerson' ams.pst >ams.err  2>&1
 $val  ../src/readpst -cv    -o output1 ams.pst                       >out1.err 2>&1
