31357dde609d44aa916201d45650c710da4bea6a
Carl Byington <carl@five-ten-sg.com>
2009-04-04 19:23:33 -0700

    new debianization from hggdh

--- debian/cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the libpst package
-#
-0 4	* * *	root	libpst_maintenance
--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(libpst):needs="X11|text|vc|wm" section="Apps/see-menu-manual"\
-  title="libpst" command="/usr/bin/libpst"
